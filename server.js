c1f6c852481ee93093a2dd221e22f851c9f14d8a
Racha <br_bia@esi.dz>
2016-07-22 12:37:33 +0100

    server updates

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express"),
 	fs = require('fs'),
 	fc = require('fs.extra'),
 	app = express(),
+	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
 	multer  = require('multer'),
 	upload = multer({ 
@@ -26,11 +27,17 @@ var express = require("express"),
 	app.set('view engine', 'html');
 	app.use("/static", express.static("public"));
 	app.use(bodyParser.urlencoded({ extended: true }));
+
+	var db = require("./models");
 	
 	//app.use(bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files')}));
+	var sessionUser = null
+	app.get("/analyze", function (req, res) {
+		res.render("index");
+	});
 
 	app.get("/", function (req, res) {
-		res.render("index");
+		res.render("pageAcceuil");
 	});
 	
 	var LineByLineReader = require('line-by-line');
@@ -80,7 +87,7 @@ var express = require("express"),
 
 	app.post("/GffData", upload.array('file', 12), function (req, res) {
 		console.log("this is the request : ", req.files);
-		fs.readFile(req.file.path, function (err, data) {
+		fs.readFile(req.files[0].path, function (err, data) {
 			if (err) {
 				throw err; 
 				res.send("ERROR");
@@ -92,7 +99,7 @@ var express = require("express"),
 				// });
 				//fs.renameSync( req.file.path, 'uploads/test' )
 				var objectFromGFF = [];
-				var lr = new LineByLineReader('uploads/'+  req.file.filename );
+				var lr = new LineByLineReader('uploads/'+  req.files[0].filename );
 				lr.on('error', function (err) {
 		   			 // 'err' contains error object
 		   			 console.log("An error occured", err.message);
@@ -248,6 +255,7 @@ var express = require("express"),
 								    var line2 = (line).split("\t");
 								    lr.pause();
 								    setTimeout(function () {
+								    	
 								    	if (line2[0]==="NONE") {
 								    		line2.splice(0,1);
 								    		line2.splice(2,1);
@@ -302,6 +310,188 @@ var express = require("express"),
 	//INDELs Treatment
 	app.post("/IndelData", upload.array('file',12), function (req, res) {
 
+	})
+
+	app.post("/signup", function (req, res) {
+		var newUser = {
+			firstName :req.body.firstname,
+			lastName : req.body.lastname,
+			userName : req.body.username,
+			email: req.body.email,
+			password: req.body.password
+		};
+		db.User.checkEmail(newUser.email, function (result) {
+			if (result === 'error') {
+				res.send("error")
+			} else if (result === "email exists") {
+				res.send("email exists")
+			} else {
+				db.User.checklogin(newUser.userName, function (result2) {
+					if (result2 === 'error') {
+						res.send("error")
+					} else if (result2 === "login exists") {
+						res.send("login exists")
+					} else {
+						db.User.createSecure(newUser.firstName, newUser.lastName, newUser.userName, newUser.password, newUser.email, function (error, newU) {
+							if (error) {
+								res.send("error")
+							} else {
+								console.log("the NEW USER IS : ", newU)
+								//req.session.user = newU;
+								sessionUser = newU;
+								//req.session._id= newU._id;
+								res.send(newU);
+							}
+						})
+					}
+				})
+			}
+		})
+	})
+
+	app.post("/login", function (req, res) {
+		var user = {
+			email : req.body.email,
+			pass  : req.body.password
+		}
+		console.log(user)
+		db.User.authenticate(user.email, user.pass, function (msg, response) {
+			if (msg === 'wrong email') {
+				console.log("wrong email")
+				res.send("wrong email")
+			}  else if (msg === "Error: incorrect password") {
+				console.log("wrong password")
+				res.send("wrong password")
+			}else {
+				console.log("login response", response)
+				sessionUser = response;
+				res.json({user : response})
+			}
+		})
+	})
+
+	app.get("/logout", function (req, res) {
+		sessionUser =null;
+		//on the logout we have to destroy the tempfile
+		res.render("pageAcceuil");
+	})
+
+	//getting the current user
+	app.get("/current_user", function (req, res) {
+		var user = sessionUser;
+		res.json({user : user})
+	})
+
+	app.get("/profile/:userName" , function (req, res) {
+		var name = req.params.userName;
+		console.log("the username received =", name)
+		var userSession = sessionUser;
+		console.log('the user session :', userSession);
+		if (sessionUser !== null) {
+			if (name !== sessionUser.userName) {
+				res.render("ErrorForbidden")
+			} else {
+				res.render('profile', {user : userSession});
+			}	
+		} else {
+			res.render("ErrorForbidden")
+		}
+	})
+
+	app.get("/:userName/upload" , function (req, res) {
+		console.log("APPEL REçuuu", req.params.userName)
+		if (req.params.userName === sessionUser.userName) {
+			console.log("page envoyée")
+			res.render('upload');
+		} else {
+			res.render("ErrorForbidden")
+		}
+	})
+
+	app.post("/:userName/update", function (req, res) {
+		var user = {
+			userName: req.body.userName,
+			email : req.body.email,
+			firstName: req.body.firstName,
+			lastName: req.body.lastName
+		}
+		userN = req.params.userName;
+		oldemail = sessionUser.email;
+		if (userN !== user.userName) {
+			db.User.checklogin(user.userName, function (msg) {
+				if (msg === "error") {
+					console.log("error niveau 1")
+					res.send("error")
+				} else if (msg ==="login exists") {
+					console.log("error niveau 2")
+					res.send("login exists")
+				} else {
+					if (oldemail !== user.email) {
+						db.User.checkEmail(user.email, function (msg) {
+							if (msg ==="error") {
+								console.log("error niveau 3")
+								res.send("error")
+							} else if (msg === "email exists") {
+								console.log("error niveau 4")
+								res.send ('email exists')
+							} else {
+								db.User.updateInfo(req.params.userName, user.userName, user.firstName, user.lastName, user.email, function (msg, response) {
+									if (msg === 'error') {
+										console.log("error niveau 5")
+										res.send("error")
+									} else {
+										console.log("updated user : ", response)
+										sessionUser = response;
+										res.json({user : response})
+									}
+								})
+							}
+						})
+					} else { 
+						db.User.updateInfo(req.params.userName, user.userName, user.firstName, user.lastName, user.email, function (msg, response) {
+							if (msg === 'error') {
+								console.log("error niveau 6")
+								res.send("error")
+							} else {
+								sessionUser = response;
+								res.json({user : response})
+							}
+						})
+
+					}
+				}
+			})
+		} else {
+			if (oldemail !== user.email) {
+						db.User.checkEmail(user.email, function (msg) {
+							if (msg ==="error") {
+
+							} else if (msg === "email exists") {
+								res.send ('email exists')
+							} else {
+								db.User.updateInfo(req.params.userName, user.userName, user.firstName, user.lastName, user.email, function (msg, response) {
+									if (msg === 'error') {
+										res.send("error")
+									} else {
+										sessionUser = response;
+										res.json({user : response})
+									}
+								})
+							}
+						})
+					} else { 
+						db.User.updateInfo(req.params.userName, user.userName, user.firstName, user.lastName, user.email, function (msg, response) {
+							if (msg === 'error') {
+								res.send("error")
+							} else {
+								sessionUser = response;
+								res.json({user : response})
+							}
+						})
+
+					}
+
+		}
 	})
  
  	// function filesTreatment(files, type) {
@@ -387,6 +577,6 @@ var express = require("express"),
 
  	// }
 
-	app.listen(process.env.PORT || 3000, function () {
-	console.log("listening on port 3000 ... success :)");
+	app.listen(process.env.PORT || 4000, function () {
+	console.log("listening on port 4000 ... success :)");
 	});
